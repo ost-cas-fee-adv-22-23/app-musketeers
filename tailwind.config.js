e957e7892dca51589782192d09b5df11f66a5267
samirmumic <sam.mumic@gmail.com>
2023-01-31 20:22:16 +0100

    feat: #61 adds initial design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [
+    require('@smartive-education/design-system-component-library-musketeers/preset'),
+  ],
   content: [
+    './node_modules/@smartive-education/design-system-component-library-musketeers/dist/components/**/*.js',
     "./app/**/*.{ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
