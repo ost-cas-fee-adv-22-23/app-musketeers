52035164e901ff625948a6a9519d1d6074a66813
André Abt <info@andre-abt.com>
2023-04-24 11:26:43 +0200

    fix: #0 Redirect from index to login when not logged in

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/mumble/:path*', '/profile/:path*', '/settings/:path*', '/results/:path*'] };
+export const config = { matcher: ['/', '/mumble/:path*', '/profile/:path*', '/settings/:path*', '/results/:path*'] };
